e0ffbaa4c1fc542c64325bd7837660f3288f575d
1E10 Laptop <sample@bths.edu>
2022-10-06 12:27:42 -0400

    Second commit

---
+++ src/TipCalculator.java
@@ -0,0 +1,22 @@
+public class TipCalculator {
+
+    // instance variables
+    private int numPeople;
+    private double tipPercentage;
+    private double totalBillBeforeTip;
+
+    //constructor
+    public TipCalculator(int numPeople, double tipPercentage){
+        this.numPeople = numPeople;
+        this.tipPercentage = tipPercentage;
+        totalBillBeforeTip = 0;
+    }
+
+    //
+
+
+
+
+
+
+}
